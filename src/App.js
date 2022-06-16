28c5cbcfad1294ee552d7e99fdd72520736a1e47
Alan Guerrero García <age_allison@hotmail.com>
2022-06-15 21:57:41 -0500

    Ejercico switch on/off

--- src/App.js
+++ src/App.js
@@ -10,6 +10,24 @@ import "./components/Fullname/buttons.css";
 
 function App() {
   const [count, setCount] = useState(0);
+
+  const handleClick = (increase) => {
+    let newCount = count;
+    if (increase) {
+      newCount = newCount + 1;
+    } else {
+      newCount = newCount - 1;
+    }
+    setCount(newCount);
+  };
+
+  const isMultiple = count % 5 === 0 && count !== 0 ? true : false;
+  const countType = isMultiple ? "Es multiplo de 5" : "No es multiplo de 5";
+  const countClass = isMultiple ? "active" : "inactivo";
+
+  const isOdd = count % 2 === 0 && count !== 0 ? true : false;
+  const countTypeOdd = isOdd ? "ON" : "OFF";
+  const countClassOdd = isOdd ? "azul" : "rojo";
   return (
     <div className="App">
       <header className="App-header">
@@ -33,6 +51,12 @@ function App() {
           <button onClick={() => setCount(count + 1)}>More</button>
           <button onClick={() => setCount(count - 1)}>Less</button>
         </div>
+        <p className={countClass}>
+          Counter: {count}, {countType}
+        </p>
+
+        <button onClick={() => handleClick(true)}>Increase</button>
+        <div className={countClassOdd}>{countTypeOdd}</div>
       </header>
     </div>
   );
